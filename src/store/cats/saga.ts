08bedd810e09e79929ed4895ec2657e25f356d92
MaxKim-J <hwaseen@gmail.com>
2021-06-21 11:07:55 +0900

    Import ActionCreatorWithPayload

--- src/store/cats/saga.ts
+++ src/store/cats/saga.ts
@@ -8,6 +8,7 @@ import { takeEvery } from "redux-saga/effects";
 import { createSaga } from "../utils";
 import { getCatFacts } from "../../api";
 
+// 프로퍼티를 객체에 명시하지 않아서 유니언타입으로 추론이 되버림
 const getCatFactsSaga = createSaga<any, CatFact[], string>(
   successFetchCatFacts,
   failFetchCatFacts,
--- src/store/utils.ts
+++ src/store/utils.ts
@@ -1,6 +1,5 @@
-import { AxiosResponse } from "axios";
 import { AsyncEntity } from "./types";
-import { PayloadAction } from "@reduxjs/toolkit";
+import { PayloadAction, ActionCreatorWithPayload } from "@reduxjs/toolkit";
 import { put, call } from "redux-saga/effects";
 
 interface CreateAsyncReducerParams {
@@ -20,31 +19,29 @@ export const createStartReducerWithoutPayload = <
   };
 };
 
-export const createAsyncReducer =
-  <State extends { [key: string]: any }>({
-    type,
-    entity
-  }: CreateAsyncReducerParams) =>
-  <R>() => {
-    switch (type) {
-      case "start":
-        return (state: State, action: PayloadAction<R>) => {
-          state[entity].status = "loading";
-        };
-      case "success":
-        return (state: State, action: PayloadAction<R>) => {
-          state[entity].data = action.payload;
-          state[entity].status = "success";
-        };
-      case "fail":
-        return (state: State, action: PayloadAction<R>) => {
-          state[entity].error = action.payload;
-          state[entity].status = "fail";
-        };
-      default:
-        return (state: State, action: PayloadAction<R>) => {};
-    }
-  };
+export const createAsyncReducer = <State extends { [key: string]: any }>({
+  type,
+  entity
+}: CreateAsyncReducerParams) => <R>() => {
+  switch (type) {
+    case "start":
+      return (state: State, action: PayloadAction<R>) => {
+        state[entity].status = "loading";
+      };
+    case "success":
+      return (state: State, action: PayloadAction<R>) => {
+        state[entity].data = action.payload;
+        state[entity].status = "success";
+      };
+    case "fail":
+      return (state: State, action: PayloadAction<R>) => {
+        state[entity].error = action.payload;
+        state[entity].status = "fail";
+      };
+    default:
+      return (state: State, action: PayloadAction<R>) => {};
+  }
+};
 
 const capitalize = (str: string) => {
   return str[0].toUpperCase() + str.slice(1);
@@ -59,46 +56,44 @@ interface CreateAsyncReducersParams {
 // 한번에 하기
 // 시나리오 보충
 // 근데 이렇게 하면 제대로 추론을 못함...
-export const createAsyncReducers =
-  <State extends { [key: string]: any }>({
-    name,
-    entity,
-    cleanDataWhenStart = false
-  }: CreateAsyncReducersParams) =>
-  <Start, Success, Fail>() => {
-    const result: {
-      [key: string]:
-        | ((state: State, action: PayloadAction<Start>) => void)
-        | ((state: State, action: PayloadAction<Success>) => void)
-        | ((state: State, action: PayloadAction<Fail>) => void);
-    } = {
-      [`${name}`]: (state: State, action: PayloadAction<Start>) => {
-        if (cleanDataWhenStart) {
-          (state[entity] as AsyncEntity<Success, Fail>).data = null;
-        }
-        (state[entity] as AsyncEntity<Success, Fail>).status = "loading";
-      },
-      [`success${capitalize(name)}`]: (
-        state: State,
-        action: PayloadAction<Success>
-      ) => {
-        (state[entity] as AsyncEntity<Success, Fail>).data = action.payload;
-        (state[entity] as AsyncEntity<Success, Fail>).status = "success";
-      },
-      [`fail${capitalize(name)}`]: (
-        state: State,
-        action: PayloadAction<Fail>
-      ) => {
-        (state[entity] as AsyncEntity<Success, Fail>).error = action.payload;
-        (state[entity] as AsyncEntity<Success, Fail>).status = "fail";
+export const createAsyncReducers = <State extends { [key: string]: any }>({
+  name,
+  entity,
+  cleanDataWhenStart = false
+}: CreateAsyncReducersParams) => <Start, Success, Fail>() => {
+  const result: {
+    [key: string]:
+      | ((state: State, action: PayloadAction<Start>) => void)
+      | ((state: State, action: PayloadAction<Success>) => void)
+      | ((state: State, action: PayloadAction<Fail>) => void);
+  } = {
+    [`${name}`]: (state: State, action: PayloadAction<Start>) => {
+      if (cleanDataWhenStart) {
+        (state[entity] as AsyncEntity<Success, Fail>).data = null;
       }
-    };
-    return result;
+      (state[entity] as AsyncEntity<Success, Fail>).status = "loading";
+    },
+    [`success${capitalize(name)}`]: (
+      state: State,
+      action: PayloadAction<Success>
+    ) => {
+      (state[entity] as AsyncEntity<Success, Fail>).data = action.payload;
+      (state[entity] as AsyncEntity<Success, Fail>).status = "success";
+    },
+    [`fail${capitalize(name)}`]: (
+      state: State,
+      action: PayloadAction<Fail>
+    ) => {
+      (state[entity] as AsyncEntity<Success, Fail>).error = action.payload;
+      (state[entity] as AsyncEntity<Success, Fail>).status = "fail";
+    }
   };
+  return result;
+};
 
 export const createSaga = <Start, Success, Fail>(
-  success: any, // 이거 추론 잘 안될듯..
-  fail: any,
+  success: ActionCreatorWithPayload<Success>,
+  fail: ActionCreatorWithPayload<Fail>,
   req: any
 ) => {
   return function* (action: PayloadAction<Start>) {
